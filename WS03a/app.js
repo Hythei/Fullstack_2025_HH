6e02e67d5d1f9afb6c851a66e0b1c0c4bdedeb95
HytHei <heikki.hyttinen16@gmail.com>
2025-02-27 16:58:29 +0200

    Added html pages and so on

--- WS03a/app.js
+++ WS03a/app.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const path = require('path');
+require('dotenv').config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
-//ex1
+app.use(express.static('./public'));
+
+
 app.listen(port,() => {
-    console.log('Port is functioning as required')
-})
+    console.log(`Server is running on port ${port}`);
+});
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
+app.get('/services', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'public', 'services.html'));
+});
+
+
+app.get("*", (req, res) => {
+    res.status(404).send("Cannot find the requested page.")
+});

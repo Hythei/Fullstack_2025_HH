cc1fa964aa85c59c3b6df26a65cf28822c2978d1
HytHei <heikki.hyttinen16@gmail.com>
2025-03-31 11:24:27 +0300

    added the last EJS task

--- WS03b_EJS/app.js
+++ WS03b_EJS/app.js
@@ -14,7 +14,7 @@ app.get("/message", (req, res) => {
   res.render("message", { name: "General Kenobi" });
 });
 
-//ex2
+//ex2 & ex5
 app.get("/", (req, res) => {
   let message =
     "Here we have an example of EJS iterating through a list of items";
@@ -22,8 +22,16 @@ app.get("/", (req, res) => {
     items: ["item1", "item2", "item3", "item4"],
   };
   let boolean_value = true;
-  res.render("index", { data: data, message: message, boolean: boolean_value });
+  let users = [
+    {name: "Laszlo", age: "30", id: "1111"},
+    {name: "Nandor", age: "31", id: "2222"},
+    {name: "Nadja", age: "32", id: "3333"},
+    {name: "Robin", age: "33", id: "4444"},
+    {name: "Guillermo", age: "34", id: "5555"},
+  ]
+  res.render("index", { data: data, message: message, boolean: boolean_value, users: users });
 });
 
 //ex3
 // Lisätty "partials" -kansioon
+

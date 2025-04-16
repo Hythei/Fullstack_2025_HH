37c797e81e5e65694b5861db006e629a50565fff
hythei <heikki.hyttinen16@gmail.com>
2025-04-16 10:34:05 +0300

    added ws05

---
+++ WSO05_REST_API/routes/opiskelijaRoutes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const Opiskelija = require('../models/opiskelija');
+
+const router = express.Router();
+
+// GET all documents
+router.get('/getall', async (req, res) => {
+  try {
+    const opiskelijat = await Opiskelija.find();
+    res.status(200).json(opiskelijat);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch items' });
+  }
+});
+
+
+//Tekase MongoDB uusi database, opiskelijat, jonne opiskelijas collection
+// GET a single document by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const opiskelija = await.Opisk
+  }
+});
+
+// POST a new document
+router.post('/add', async (req, res) => {
+  res.status(200).json({ message: 'POST request received' });
+});
+
+// PATCH (update) a document by ID
+router.patch('/update/:id', async (req, res) => {
+  res.status(200).json({ message: 'PATCH request received' });
+});
+
+// DELETE a document by ID
+router.delete('/delete/:id', async (req, res) => {
+res.status(200).json({ message: 'DELETE request received' });
+});
+
+module.exports = router;

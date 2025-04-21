46f462463b49dfd52dc9e55c39cd5f5dcb9da21e
HytHei <heikki.hyttinen16@gmail.com>
2025-04-21 13:25:50 +0300

    progress

--- WSO05_REST_API/routes/opiskelijaRoutes.js
+++ WSO05_REST_API/routes/opiskelijaRoutes.js
@@ -8,23 +8,36 @@ router.get('/getall', async (req, res) => {
   try {
     const opiskelijat = await Opiskelija.find();
     res.status(200).json(opiskelijat);
+    console.log(opiskelijat);
+    
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch items' });
   }
 });
 
 
-//Tekase MongoDB uusi database, opiskelijat, jonne opiskelijas collection
 // GET a single document by ID
-router.get('/:id', async (req, res) => {
-  try {
-    const opiskelija = await.Opisk
-  }
-});
+// router.get('/:id', async (req, res) => {
+//   try {
+//     const opiskelija = await.Opisk
+//   }
+// });
 
 // POST a new document
 router.post('/add', async (req, res) => {
-  res.status(200).json({ message: 'POST request received' });
+  const newData = new Opiskelija({
+    name: req.body.name,
+    email: req.body.email,
+    age: req.body.age,
+    group:req.body.group,
+  })
+  try {
+    const dataToSave = await newData.save();
+    res.status(200).json(dataToSave)
+  }
+  catch (error){
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // PATCH (update) a document by ID
